2f7bf449946ff61a4d6c63bd0ba804ac76012069
Andreas Zweili <andreas@2li.ch>
2017-10-30 20:43:29 +0100

    add diagrammes to the repo

---
+++ docs/diagrammes/mindmap/webshop.mm
@@ -0,0 +1,92 @@
+<map version="1.0.0"><node ID="BC5670F1-9B93-4A3F-85B0-5C1CEBD0097C" BACKGROUND_COLOR="#FFFFFF" TEXT="webshop" COLOR="#195468" POSITION="right" STYLE="bubble"><edge COLOR="#1A5469" WIDTH="4" /><font NAME="HelveticaNeue-Light" SIZE="24" /><node ID="1F04D5E3-DFB5-4966-A589-839E8A7C1647" TEXT="Navigation" COLOR="#425A65" POSITION="right" STYLE="fork"><edge COLOR="#25BAD1" WIDTH="6" /><font NAME="HelveticaNeue" SIZE="18" /><node ID="DBB00329-E197-4412-8C51-D84B152B3FF7" TEXT="Produktkategorie" COLOR="#425A65" POSITION="right" STYLE="fork"><edge COLOR="#25BAD1" WIDTH="5" /><font NAME="HelveticaNeue" SIZE="18" /></node>
+<node ID="1BCA7DF3-49DB-49D0-957C-E40AF37FC2D2" TEXT="Produkte" COLOR="#425A65" POSITION="right" STYLE="fork"><edge COLOR="#25BAD1" WIDTH="5" /><font NAME="HelveticaNeue" SIZE="18" /></node>
+</node>
+<node ID="B409B7FB-71F4-4D89-B6AF-4726AF3B5A11" TEXT="Warenkorb" COLOR="#425A65" POSITION="right" STYLE="fork"><edge COLOR="#389237" WIDTH="6" /><font NAME="HelveticaNeue" SIZE="18" /><node ID="F74B8CC9-21E1-41AD-B016-1787BA6AEFDF" TEXT="Liste" COLOR="#425A65" POSITION="right" STYLE="fork"><edge COLOR="#389237" WIDTH="5" /><font NAME="HelveticaNeue" SIZE="18" /><node ID="EFABB56D-7DD5-451F-8D14-780CD2CC23F4" TEXT="Preis" COLOR="#425A65" POSITION="right" STYLE="fork"><edge COLOR="#389237" WIDTH="4" /><font NAME="HelveticaNeue" SIZE="18" /></node>
+<node ID="214FF92F-CA70-45C3-959A-E67D5CCF5696" TEXT="Menge" COLOR="#425A65" POSITION="right" STYLE="fork"><edge COLOR="#389237" WIDTH="4" /><font NAME="HelveticaNeue" SIZE="18" /><node ID="486660D0-444B-4A30-A244-F949E5AA84D5" TEXT="&#196;ndern" COLOR="#425A65" POSITION="right" STYLE="fork"><edge COLOR="#389237" WIDTH="3" /><font NAME="HelveticaNeue" SIZE="18" /></node>
+<node ID="BDC4237F-6649-4827-B99C-7612E8B516E0" TEXT="L&#246;schen" COLOR="#425A65" POSITION="right" STYLE="fork"><edge COLOR="#389237" WIDTH="3" /><font NAME="HelveticaNeue" SIZE="18" /></node>
+</node>
+</node>
+<node ID="872F794C-6558-4DA1-A5AA-C098D1E4BF07" TEXT="zur Kasse" COLOR="#425A65" POSITION="right" STYLE="fork"><edge COLOR="#389237" WIDTH="5" /><font NAME="HelveticaNeue" SIZE="18" /><node ID="03CFECBB-2242-4A55-8496-A0D22690316D" TEXT="Adresse" COLOR="#425A65" POSITION="right" STYLE="fork"><edge COLOR="#389237" WIDTH="4" /><font NAME="HelveticaNeue" SIZE="18" /></node>
+<node ID="3CB01FA8-3C3D-4896-928E-7EF94AE1205E" TEXT="Bestellung ausl&#246;sen" COLOR="#425A65" POSITION="right" STYLE="fork"><edge COLOR="#389237" WIDTH="4" /><font NAME="HelveticaNeue" SIZE="18" /><node ID="C2FCF0BE-7C4E-40C5-BDBE-C58C9F6366F5" TEXT="Bestelldatum" COLOR="#425A65" POSITION="right" STYLE="fork"><edge COLOR="#389237" WIDTH="3" /><font NAME="HelveticaNeue" SIZE="18" /></node>
+<node ID="EE80BAF0-6AA6-4155-B4B4-E0FB8895D56B" TEXT="Kundendaten" COLOR="#425A65" POSITION="right" STYLE="fork"><edge COLOR="#389237" WIDTH="3" /><font NAME="HelveticaNeue" SIZE="18" /></node>
+<node ID="5251A5B8-F511-4043-BAED-E955E9B2D198" TEXT="Warenkorb" COLOR="#425A65" POSITION="right" STYLE="fork"><edge COLOR="#389237" WIDTH="3" /><font NAME="HelveticaNeue" SIZE="18" /></node>
+</node>
+<node ID="CF52AFDF-23A1-45D9-A21B-6F6C0BE55358" TEXT="email" COLOR="#425A65" POSITION="right" STYLE="fork"><edge COLOR="#389237" WIDTH="4" /><font NAME="HelveticaNeue" SIZE="18" /></node>
+</node>
+<node ID="D69872F3-B15B-40A4-94CF-8639E07DD98C" TEXT="Total" COLOR="#425A65" POSITION="right" STYLE="fork"><edge COLOR="#389237" WIDTH="5" /><font NAME="HelveticaNeue" SIZE="18" /></node>
+</node>
+<node ID="06381F24-0D04-495D-8495-8200DB27CE5B" TEXT="Artikel" COLOR="#425A65" POSITION="right" STYLE="fork"><edge COLOR="#1286DE" WIDTH="6" /><font NAME="HelveticaNeue" SIZE="18" /><node ID="89105CD8-415B-46D5-9A2C-40318F151B1C" TEXT="Attribute" COLOR="#425A65" POSITION="right" STYLE="fork"><edge COLOR="#1286DE" WIDTH="5" /><font NAME="HelveticaNeue" SIZE="18" /><node ID="8E742C41-8E15-4663-99D7-6C72B310C385" TEXT="ProduktID" COLOR="#425A65" POSITION="right" STYLE="fork"><edge COLOR="#1286DE" WIDTH="4" /><font NAME="HelveticaNeue" SIZE="18" /></node>
+<node ID="1B584828-A2A5-4E5D-8C50-9BC2DC26D545" TEXT="Bezeichnung" COLOR="#425A65" POSITION="right" STYLE="fork"><edge COLOR="#1286DE" WIDTH="4" /><font NAME="HelveticaNeue" SIZE="18" /></node>
+<node ID="5536CDA3-618B-4F80-B035-5C12B685A5A1" TEXT="Beschreibung" COLOR="#425A65" POSITION="right" STYLE="fork"><edge COLOR="#1286DE" WIDTH="4" /><font NAME="HelveticaNeue" SIZE="18" /></node>
+<node ID="52B19E03-237E-4FA7-9E6B-462BDCF77DF9" TEXT="Bilder" COLOR="#425A65" POSITION="right" STYLE="fork"><edge COLOR="#1286DE" WIDTH="4" /><font NAME="HelveticaNeue" SIZE="18" /></node>
+<node ID="EBB22E03-AE85-4395-B7A5-F7557E66FDED" TEXT="Status" COLOR="#425A65" POSITION="right" STYLE="fork"><edge COLOR="#1286DE" WIDTH="4" /><font NAME="HelveticaNeue" SIZE="18" /></node>
+</node>
+<node ID="95F7D3AE-BD89-4417-9513-7839C3206E2E" TEXT="W&#228;hrung" COLOR="#425A65" POSITION="right" STYLE="fork"><edge COLOR="#1286DE" WIDTH="5" /><font NAME="HelveticaNeue" SIZE="18" /><node ID="F2853290-FD56-4AE6-A35A-31863EE258BE" TEXT="CHF" COLOR="#425A65" POSITION="right" STYLE="fork"><edge COLOR="#1286DE" WIDTH="4" /><font NAME="HelveticaNeue" SIZE="18" /></node>
+<node ID="6EF217D4-BA26-4F91-B96B-636AA3D17F77" TEXT="JPE" COLOR="#425A65" POSITION="right" STYLE="fork"><edge COLOR="#1286DE" WIDTH="4" /><font NAME="HelveticaNeue" SIZE="18" /></node>
+<node ID="2E82CBD8-ADB5-4A81-916B-E8DB953D416E" TEXT="Euro" COLOR="#425A65" POSITION="right" STYLE="fork"><edge COLOR="#1286DE" WIDTH="4" /><font NAME="HelveticaNeue" SIZE="18" /></node>
+<node ID="8A142F3E-C432-4CB5-B168-C7398A74E16F" TEXT="Dollar" COLOR="#425A65" POSITION="right" STYLE="fork"><edge COLOR="#1286DE" WIDTH="4" /><font NAME="HelveticaNeue" SIZE="18" /></node>
+</node>
+<node ID="F297970B-5613-48B0-A431-9F2BE5C0D2F1" TEXT="Lager" COLOR="#425A65" POSITION="right" STYLE="fork"><edge COLOR="#1286DE" WIDTH="5" /><font NAME="HelveticaNeue" SIZE="18" /><node ID="4A787E22-3EBE-4008-AE4C-05EC53946173" TEXT="Lagerbestand" COLOR="#425A65" POSITION="right" STYLE="fork"><edge COLOR="#1286DE" WIDTH="4" /><font NAME="HelveticaNeue" SIZE="18" /></node>
+<node ID="209505F4-1590-4457-A2C3-FDAE7DFF40A0" TEXT="Nachbestellung" COLOR="#425A65" POSITION="right" STYLE="fork"><edge COLOR="#1286DE" WIDTH="4" /><font NAME="HelveticaNeue" SIZE="18" /><node ID="242CD8BB-C51A-45B4-8E2D-3FCB44FA775E" TEXT="Lieferdatum" COLOR="#425A65" POSITION="right" STYLE="fork"><edge COLOR="#1286DE" WIDTH="3" /><font NAME="HelveticaNeue" SIZE="18" /></node>
+<node ID="536B154C-948B-486A-9FCC-0A913FD1F707" TEXT="Bestellmenge" COLOR="#425A65" POSITION="right" STYLE="fork"><edge COLOR="#1286DE" WIDTH="3" /><font NAME="HelveticaNeue" SIZE="18" /></node>
+</node>
+</node>
+<node ID="A5EB7FD7-5BEC-488D-A5AD-516631D86C4D" TEXT="Produktkategorie" COLOR="#425A65" POSITION="right" STYLE="fork"><edge COLOR="#1286DE" WIDTH="5" /><font NAME="HelveticaNeue" SIZE="18" /><node ID="BB861A53-EC5D-4DB0-8250-743B2BED7F7E" TEXT="Level 1" COLOR="#425A65" POSITION="right" STYLE="fork"><edge COLOR="#1286DE" WIDTH="4" /><font NAME="HelveticaNeue" SIZE="18" /></node>
+<node ID="30045F6A-6D26-4855-8225-979F315A323B" TEXT="Level 2" COLOR="#425A65" POSITION="right" STYLE="fork"><edge COLOR="#1286DE" WIDTH="4" /><font NAME="HelveticaNeue" SIZE="18" /></node>
+</node>
+</node>
+<node ID="97C52BFD-7D88-43C6-B536-578CDA97F942" TEXT="Backend" COLOR="#425A65" POSITION="right" STYLE="fork"><edge COLOR="#DC2128" WIDTH="6" /><font NAME="HelveticaNeue" SIZE="18" /><node ID="8DD55F98-CBAD-4032-82AF-C219AD649903" TEXT="Produkte" COLOR="#425A65" POSITION="right" STYLE="fork"><edge COLOR="#DC2128" WIDTH="5" /><font NAME="HelveticaNeue" SIZE="18" /><node ID="B9CF3087-4113-4FEA-9B06-8E8413C7DD97" TEXT="Produktdetails" COLOR="#425A65" POSITION="right" STYLE="fork"><edge COLOR="#DC2128" WIDTH="4" /><font NAME="HelveticaNeue" SIZE="18" /><node ID="8ED866BB-3441-4CC2-AA13-9D3D6380BAA6" TEXT="Attribute" COLOR="#425A65" POSITION="right" STYLE="fork"><edge COLOR="#DC2128" WIDTH="3" /><font NAME="HelveticaNeue" SIZE="18" /></node>
+<node ID="AC323EB4-7D6D-464E-8097-FF5DFAA17E65" TEXT="" COLOR="#425A65" POSITION="right" STYLE="fork"><edge COLOR="#DC2128" WIDTH="3" /><font NAME="HelveticaNeue" SIZE="18" /></node>
+</node>
+<node ID="36D8DA3C-9EC2-4DE1-AB6B-40C79B77AACE" TEXT="" COLOR="#425A65" POSITION="right" STYLE="fork"><edge COLOR="#DC2128" WIDTH="4" /><font NAME="HelveticaNeue" SIZE="18" /></node>
+</node>
+<node ID="9293F7AA-8D79-4277-9CE9-B4EC4D3AD676" TEXT="Bestellungen" COLOR="#425A65" POSITION="right" STYLE="fork"><edge COLOR="#DC2128" WIDTH="5" /><font NAME="HelveticaNeue" SIZE="18" /><node ID="E4957A6F-9C8A-4470-A81D-53BD32CAA3E6" TEXT="" COLOR="#425A65" POSITION="right" STYLE="fork"><edge COLOR="#DC2128" WIDTH="4" /><font NAME="HelveticaNeue" SIZE="18" /></node>
+<node ID="7EC34120-C1F8-4F7D-8967-106379A37B6C" TEXT="Bestellung" COLOR="#425A65" POSITION="right" STYLE="fork"><edge COLOR="#DC2128" WIDTH="4" /><font NAME="HelveticaNeue" SIZE="18" /><node ID="10CD9D95-C8AF-4963-90FA-4B58F1BEBADD" TEXT="Bestelldetails" COLOR="#425A65" POSITION="right" STYLE="fork"><edge COLOR="#DC2128" WIDTH="3" /><font NAME="HelveticaNeue" SIZE="18" /></node>
+<node ID="A9E66192-159A-4149-A331-7D3113EC540E" TEXT="" COLOR="#425A65" POSITION="right" STYLE="fork"><edge COLOR="#DC2128" WIDTH="3" /><font NAME="HelveticaNeue" SIZE="18" /></node>
+</node>
+</node>
+</node>
+<node ID="DA3A8E2B-CA6C-46D7-A697-B86B4DC94A96" TEXT="sekund&#228;re Ziele" COLOR="#425A65" POSITION="left" STYLE="fork"><edge COLOR="#FCB60D" WIDTH="6" /><font NAME="HelveticaNeue" SIZE="18" /><node ID="268D3CC1-CD82-4DD3-BC74-B885F3BD7BA6" TEXT="Einfachheit" COLOR="#425A65" POSITION="left" STYLE="fork"><edge COLOR="#FCB60D" WIDTH="5" /><font NAME="HelveticaNeue" SIZE="18" /></node>
+<node ID="14882776-E58E-43C3-85A9-F7590D823CA9" TEXT="Antworzeit &#60; 3 sek." COLOR="#425A65" POSITION="left" STYLE="fork"><edge COLOR="#FCB60D" WIDTH="5" /><font NAME="HelveticaNeue" SIZE="18" /></node>
+<node ID="E697AF25-18A1-4AD6-8ABD-A7A18645A837" TEXT="SQL Injection Immun" COLOR="#425A65" POSITION="left" STYLE="fork"><edge COLOR="#FCB60D" WIDTH="5" /><font NAME="HelveticaNeue" SIZE="18" /></node>
+<node ID="0276577F-BC2A-4CFB-8613-E78F67ACDD12" TEXT="XSS Attacken Immun" COLOR="#425A65" POSITION="left" STYLE="fork"><edge COLOR="#FCB60D" WIDTH="5" /><font NAME="HelveticaNeue" SIZE="18" /></node>
+<node ID="CB094FF2-B51C-4F00-87D3-5D8D1231CA5E" TEXT="lauff&#228;hig" COLOR="#425A65" POSITION="left" STYLE="fork"><edge COLOR="#FCB60D" WIDTH="5" /><font NAME="HelveticaNeue" SIZE="18" /><node ID="85F6FCE4-0B5D-4DE6-8B6E-4E182B6AEA9D" TEXT="Linux" COLOR="#425A65" POSITION="left" STYLE="fork"><edge COLOR="#FCB60D" WIDTH="4" /><font NAME="HelveticaNeue" SIZE="18" /></node>
+<node ID="B6F887A7-EC7B-45B1-82AD-624A4690DF88" TEXT="MacOS" COLOR="#425A65" POSITION="left" STYLE="fork"><edge COLOR="#FCB60D" WIDTH="4" /><font NAME="HelveticaNeue" SIZE="18" /></node>
+<node ID="9EA2EA36-8748-471C-A895-99638E3F54CE" TEXT="Windows" COLOR="#425A65" POSITION="left" STYLE="fork"><edge COLOR="#FCB60D" WIDTH="4" /><font NAME="HelveticaNeue" SIZE="18" /></node>
+</node>
+</node>
+<node ID="E8C9081A-6A8F-4A89-A784-17114C83750C" TEXT="Projekt" COLOR="#425A65" POSITION="left" STYLE="fork"><edge COLOR="#922AA4" WIDTH="6" /><font NAME="HelveticaNeue" SIZE="18" /><node ID="6D18C19D-4D5E-473B-B93C-B35264749485" TEXT="Vorgehensmodell" COLOR="#425A65" POSITION="left" STYLE="fork"><edge COLOR="#922AA4" WIDTH="5" /><font NAME="HelveticaNeue" SIZE="18" /></node>
+<node ID="FF07607E-051E-4754-B537-CBB3965078D5" TEXT="Arbeitspacketdefinition" COLOR="#425A65" POSITION="left" STYLE="fork"><edge COLOR="#922AA4" WIDTH="5" /><font NAME="HelveticaNeue" SIZE="18" /></node>
+<node ID="5522ADF2-90D5-4014-B422-E41BBE6CC393" TEXT="Architekturbeschreibung" COLOR="#425A65" POSITION="left" STYLE="fork"><edge COLOR="#922AA4" WIDTH="5" /><font NAME="HelveticaNeue" SIZE="18" /><node ID="8F47A5E3-9F66-4299-A19E-3386DC5F1873" TEXT="Technologie" COLOR="#425A65" POSITION="left" STYLE="fork"><edge COLOR="#922AA4" WIDTH="4" /><font NAME="HelveticaNeue" SIZE="18" /></node>
+<node ID="406E2D83-9C3E-40B4-A366-16AE4DD9654F" TEXT="Komponenten" COLOR="#425A65" POSITION="left" STYLE="fork"><edge COLOR="#922AA4" WIDTH="4" /><font NAME="HelveticaNeue" SIZE="18" /></node>
+</node>
+<node ID="6A0978C9-ED15-4643-BCF0-C4B40BE8CFDE" TEXT="Zwischenstand aufzeigen Soll/Ist" COLOR="#425A65" POSITION="left" STYLE="fork"><edge COLOR="#922AA4" WIDTH="5" /><font NAME="HelveticaNeue" SIZE="18" /></node>
+<node ID="7E61E4DD-F8CC-47C1-8FD6-B174130D80D6" TEXT="Zeitplanung" COLOR="#425A65" POSITION="left" STYLE="fork"><edge COLOR="#922AA4" WIDTH="5" /><font NAME="HelveticaNeue" SIZE="18" /><node ID="A0E3747D-1A6F-44DA-A3A9-DFD0E297057A" TEXT="W&#246;chentlich" COLOR="#425A65" POSITION="left" STYLE="fork"><edge COLOR="#922AA4" WIDTH="4" /><font NAME="HelveticaNeue" SIZE="18" /></node>
+<node ID="C84BF307-98DE-4865-A501-7B3CDE21D28F" TEXT="Wer wie lange" COLOR="#425A65" POSITION="left" STYLE="fork"><edge COLOR="#922AA4" WIDTH="4" /><font NAME="HelveticaNeue" SIZE="18" /></node>
+</node>
+<node ID="C20EB525-D3D6-4FBB-AE1F-6F0F57154AC4" TEXT="Milestones" COLOR="#425A65" POSITION="left" STYLE="fork"><edge COLOR="#922AA4" WIDTH="5" /><font NAME="HelveticaNeue" SIZE="18" /></node>
+<node ID="5D9C21C8-F08A-46B2-AACF-AC49564951F1" TEXT="Pr&#228;sentation" COLOR="#425A65" POSITION="left" STYLE="fork"><edge COLOR="#922AA4" WIDTH="5" /><font NAME="HelveticaNeue" SIZE="18" /><node ID="F053CC30-301E-41C5-8B53-B439673D56F6" TEXT="Ausgangslage" COLOR="#425A65" POSITION="left" STYLE="fork"><edge COLOR="#922AA4" WIDTH="4" /><font NAME="HelveticaNeue" SIZE="18" /></node>
+<node ID="A0C2CA92-DE96-426C-946D-4B5C2356194E" TEXT="L&#246;sungsweg" COLOR="#425A65" POSITION="left" STYLE="fork"><edge COLOR="#922AA4" WIDTH="4" /><font NAME="HelveticaNeue" SIZE="18" /></node>
+<node ID="304520A5-9408-480D-92FA-60374BA5A7A7" TEXT="Begr&#252;ndung f&#252;r die gew&#228;hlte L&#246;sung" COLOR="#425A65" POSITION="left" STYLE="fork"><edge COLOR="#922AA4" WIDTH="4" /><font NAME="HelveticaNeue" SIZE="18" /></node>
+<node ID="71906644-9E2D-48D7-9F13-DAA0D6BA289C" TEXT="Vorteile, Kosten und Nutzen aufzeigen" COLOR="#425A65" POSITION="left" STYLE="fork"><edge COLOR="#922AA4" WIDTH="4" /><font NAME="HelveticaNeue" SIZE="18" /></node>
+</node>
+<node ID="CB307372-9444-4FBD-A3D4-D48AE9E48AAE" TEXT="Dokumentation" COLOR="#425A65" POSITION="left" STYLE="fork"><edge COLOR="#922AA4" WIDTH="5" /><font NAME="HelveticaNeue" SIZE="18" /><node ID="2E2DD31C-D3D8-4C4C-ABE2-D2E853FDF1AF" TEXT="Soll/Ist" COLOR="#425A65" POSITION="left" STYLE="fork"><edge COLOR="#922AA4" WIDTH="4" /><font NAME="HelveticaNeue" SIZE="18" /></node>
+<node ID="765058CB-E349-4C9B-A968-CA55971BC0B9" TEXT="Erreichte Ziele" COLOR="#425A65" POSITION="left" STYLE="fork"><edge COLOR="#922AA4" WIDTH="4" /><font NAME="HelveticaNeue" SIZE="18" /></node>
+<node ID="DE404830-2C0F-4A9D-A38F-245A3B7602A6" TEXT="DB Design" COLOR="#425A65" POSITION="left" STYLE="fork"><edge COLOR="#922AA4" WIDTH="4" /><font NAME="HelveticaNeue" SIZE="18" /><node ID="5325F407-A810-4D5B-BE19-FB1A94E2DDED" TEXT="ER" COLOR="#425A65" POSITION="left" STYLE="fork"><edge COLOR="#922AA4" WIDTH="3" /><font NAME="HelveticaNeue" SIZE="18" /></node>
+<node ID="8181293B-A16D-44D2-8E11-EB0B7D8C393B" TEXT="ERM" COLOR="#425A65" POSITION="left" STYLE="fork"><edge COLOR="#922AA4" WIDTH="3" /><font NAME="HelveticaNeue" SIZE="18" /></node>
+</node>
+<node ID="AEA136DA-34E2-4E2C-B63F-D3ECBE22B048" TEXT="SW Design" COLOR="#425A65" POSITION="left" STYLE="fork"><edge COLOR="#922AA4" WIDTH="4" /><font NAME="HelveticaNeue" SIZE="18" /><node ID="1237C18C-12C5-4101-8CFC-CDF64962F5DD" TEXT="UML Diagramme" COLOR="#425A65" POSITION="left" STYLE="fork"><edge COLOR="#922AA4" WIDTH="3" /><font NAME="HelveticaNeue" SIZE="18" /></node>
+<node ID="3FC9F73B-5EB4-4B15-9DEA-615BF7F149D3" TEXT="Beschreibung der wichtigen Funktionen" COLOR="#425A65" POSITION="left" STYLE="fork"><edge COLOR="#922AA4" WIDTH="3" /><font NAME="HelveticaNeue" SIZE="18" /></node>
+</node>
+<node ID="2846CD46-8C60-408D-857E-2FF1FA203914" TEXT="Gelerntes aufzeigen" COLOR="#425A65" POSITION="left" STYLE="fork"><edge COLOR="#922AA4" WIDTH="4" /><font NAME="HelveticaNeue" SIZE="18" /></node>
+<node ID="C8622A5C-BC28-4B17-9A20-2ACB5C818807" TEXT="Positive/Negative Aspekte der Arbeit" COLOR="#425A65" POSITION="left" STYLE="fork"><edge COLOR="#922AA4" WIDTH="4" /><font NAME="HelveticaNeue" SIZE="18" /></node>
+</node>
+<node ID="40079051-C5BE-41C5-A9AF-1F4C38CB7DA0" TEXT="Abgaben" COLOR="#425A65" POSITION="left" STYLE="fork"><edge COLOR="#922AA4" WIDTH="5" /><font NAME="HelveticaNeue" SIZE="18" /><node ID="DE7F6BF5-01D1-4F84-8C42-31388EB4BAB4" TEXT="Applikation" COLOR="#425A65" POSITION="left" STYLE="fork"><edge COLOR="#922AA4" WIDTH="4" /><font NAME="HelveticaNeue" SIZE="18" /><node ID="5DE58F7D-B719-4DEC-AE24-B79F5591A746" TEXT="Quelltext mit Framework" COLOR="#425A65" POSITION="left" STYLE="fork"><edge COLOR="#922AA4" WIDTH="3" /><font NAME="HelveticaNeue" SIZE="18" /></node>
+<node ID="ED20B43F-C67D-4E93-BA9F-8D3CEC0A313B" TEXT="Datenbankdump" COLOR="#425A65" POSITION="left" STYLE="fork"><edge COLOR="#922AA4" WIDTH="3" /><font NAME="HelveticaNeue" SIZE="18" /></node>
+<node ID="EA6E6C4F-4BA8-4325-A9AD-C7693AFB93F1" TEXT="Ausf&#252;hrbare Version" COLOR="#425A65" POSITION="left" STYLE="fork"><edge COLOR="#922AA4" WIDTH="3" /><font NAME="HelveticaNeue" SIZE="18" /></node>
+</node>
+<node ID="09FB4E47-8896-4F35-A6D2-26F73DAA74F6" TEXT="URL zu lauff&#228;higen Version" COLOR="#425A65" POSITION="left" STYLE="fork"><edge COLOR="#922AA4" WIDTH="4" /><font NAME="HelveticaNeue" SIZE="18" /></node>
+</node>
+</node>
+</node>
+</map>
