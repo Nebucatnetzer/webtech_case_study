25d0c6f2b0a22bc55a641598f3f2d8ae02d1f920
Andreas Zweili <andreas@2li.ch>
2017-11-12 21:02:40 +0100

    Revert "rename the django project"
    This reverts commit 9e88888750674d65285647ef66b70309570c6703.

--- django/webshop/webshop/urls.py
+++ django/webshop/webshop/urls.py
@@ -1,4 +1,4 @@
-"""didgeridoo URL Configuration
+"""webshop URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.10/topics/http/urls/
@@ -18,5 +18,4 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^webshop/', include('webshop.urls')),
 ]
