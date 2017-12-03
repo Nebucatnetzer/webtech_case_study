a3ffae1ad346b32fd02c4a9ebeb09aa817fc356b
Andreas Zweili <andreas@2li.ch>
2017-12-03 17:00:57 +0100

    add django-extensions for generating the diagrammes

--- django/didgeridoo/didgeridoo/settings.py
+++ django/didgeridoo/didgeridoo/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'webshop.apps.WebshopConfig',
+    'django_extensions',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
