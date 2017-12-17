cc313a50f31acac509f595d0dd475df0f79e002f
Andreas Zweili <andreas@2li.ch>
2017-12-17 10:35:21 +0100

    add an index view and a coresponding template

--- django/didgeridoo/webshop/views.py
+++ django/didgeridoo/webshop/views.py
@@ -1,8 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse
+from django.template import loader
+
+from .models import Article, Category, ArticleStatus
 
 # Create your views here.
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    parent_category_list = Category.objects.filter(parent_category=None)
+    category_list = {}
+
+    for i in parent_category_list:
+            category = Category.objects.filter(parent_category=i.id)
+            category_list[i] = category
+
+    template = loader.get_template('webshop/index.html')
+    context = {
+        'category_list': category_list,
+        'parent_category_list': parent_category_list,
+    }
+    return HttpResponse(template.render(context, request))
