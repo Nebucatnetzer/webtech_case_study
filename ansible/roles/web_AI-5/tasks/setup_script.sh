16a854012deed327637efb0487dd0cdd9447cc67
Andreas Zweili <andreas@2li.ch>
2017-12-25 22:47:04 +0100

    move some parts from the vagrant file into a separate script
    This way both vagrant and ansible can use the script

--- ansible/roles/web_AI-5/tasks/setup_script.sh
+++ ansible/roles/web_AI-5/tasks/setup_script.sh
@@ -1,6 +1,19 @@
+#initialize the db
 mysql < /vagrant/sql/04_remove_database.sql
 mysql < /vagrant/sql/01_create_database.sql
+
+#remove old migrations
 rm /vagrant/django/didgeridoo/webshop/migrations/*.py
+
+#create and insert the new migrations
 python3 /vagrant/django/didgeridoo/manage.py makemigrations webshop
 python3 /vagrant/django/didgeridoo/manage.py migrate
+
+#insert some default data into the database
 mysql < /vagrant/sql/02_insert_data.sql
+
+#create an admin user
+echo "from django.contrib.auth.models import User; \
+      User.objects.filter(email='admin@example.com').delete(); \
+      User.objects.create_superuser('admin', 'admin@example.com', 'password')" |
+      python3 /vagrant/django/didgeridoo/manage.py shell
