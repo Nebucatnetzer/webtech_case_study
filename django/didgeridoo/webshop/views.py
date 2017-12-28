f9f1c89a015d9d3cd092f1bd087412c15db3912e
Andreas Zweili <andreas@2li.ch>
2017-12-28 16:40:04 +0100

    make imports more explicit

--- django/didgeridoo/webshop/views.py
+++ django/didgeridoo/webshop/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import get_object_or_404, render
-from .models import Article, Category, ArticleStatus
+from webshop.models import Article, Category, ArticleStatus
 
 # Create your views here.
 
