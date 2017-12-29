d500712dc2635417cfd806c29b669b6131a28eb8
Ivan Hörler <i.hoerler@me.com>
2017-12-29 22:32:27 +0100

    changed some of the views and implemented a more selfish perspective of a shop. For shure not ideal and in NO way DRY but a solution for now.

--- django/didgeridoo/webshop/views.py
+++ django/didgeridoo/webshop/views.py
@@ -12,6 +12,8 @@ from webshop.forms import RegistrationForm
 def index(request):
     parent_category_list = Category.objects.filter(parent_category=None)
     category_list = {}
+    hidden = ArticleStatus.objects.get(name="hidden")
+    articles_list = Article.objects.all().exclude(status=hidden.id)
 
     for i in parent_category_list:
             category_list.update(
@@ -19,7 +21,8 @@ def index(request):
 
     return render(request,
                   'webshop/index.html',
-                  {'category_list': category_list})
+                  {'category_list': category_list,
+                   'articles_list': articles_list})
 
 
 def articles_in_category(request, category_id):
@@ -29,16 +32,33 @@ def articles_in_category(request, category_id):
     article_list = Article.objects.filter(
         category=selected_category.id).exclude(status=hidden.id)
 
+    parent_category_list = Category.objects.filter(parent_category=None)
+    category_list = {}
+
+    for i in parent_category_list:
+            category_list.update(
+                {i: Category.objects.filter(parent_category=i.id)})
+
     return render(request, 'webshop/category.html',
                   {'article_list': article_list,
+                   'category_list': category_list,
                    'category': selected_category})
 
 
 def article_details(request, article_id):
-        article = get_object_or_404(Article, pk=article_id)
-        return render(request, 'webshop/article_details.html',
-                      {'article': article})
 
+    parent_category_list = Category.objects.filter(parent_category=None)
+    category_list = {}
+
+    for i in parent_category_list:
+            category_list.update(
+                {i: Category.objects.filter(parent_category=i.id)})
+
+    article = get_object_or_404(Article, pk=article_id)
+
+    return render(request, 'webshop/article_details.html',
+                  {'article': article,
+                   'category_list': category_list})
 
 @login_required
 def profile(request):
