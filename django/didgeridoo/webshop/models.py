e54765a54fa1e3f641ed9189c63f78992ca0720a
Andreas Zweili <andreas@2li.ch>
2018-01-14 12:22:46 +0100

    remove the delete option from the picture model
    This option got added by accident. If we would keep it then the
    related article would get deleted when one deletes a picture.

--- django/didgeridoo/webshop/models.py
+++ django/didgeridoo/webshop/models.py
@@ -68,8 +68,8 @@ class OrderOfGoods(models.Model):
 
 
 class Picture(models.Model):
-    article = models.ForeignKey(Article, on_delete=models.CASCADE)
     name = models.CharField(max_length=200)
+    article = models.ForeignKey(Article)
     image = models.ImageField(upload_to="images")
 
     def __str__(self):
