9c23d667c80bca13057df73129f4463e41df6188
Andreas Zweili <andreas@2li.ch>
2018-01-28 22:04:48 +0100

    rename the empty_label to CHF

--- django/didgeridoo/currencies/forms.py
+++ django/didgeridoo/currencies/forms.py
@@ -5,4 +5,4 @@ from currencies.models import ExchangeRate_name
 class CurrenciesForm(forms.Form):
     currencies = forms.ModelChoiceField(
         queryset=ExchangeRate_name.objects.all(),
-                required=False)
+                required=False, empty_label='CHF')
