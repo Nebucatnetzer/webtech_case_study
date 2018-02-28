1e577f4520bb4b5a35ee56bb4226891946d8cb62
Ivan Hörler <i.hoerler@me.com>
2018-02-28 01:23:20 +0100

    improve dropdown

--- django/didgeridoo/currencies/forms.py
+++ django/didgeridoo/currencies/forms.py
@@ -5,4 +5,13 @@ from currencies.models import ExchangeRate_name
 class CurrenciesForm(forms.Form):
     currencies = forms.ModelChoiceField(
         queryset=ExchangeRate_name.objects.all(),
-                required=False, empty_label='CHF')
+        required=False,
+        empty_label='CHF',
+        label='CURENCIES',
+        widget=forms.Select(
+            attrs={
+                'onchange': 'currency.submit();',
+                'class': 'btn-primary dropdown-toggle'
+            }
+        )
+    )
